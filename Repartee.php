21eb542187b34d102a7b9b967ceb3f3db7ae76dc
Mark Greenall <mark@markgreenall.uk>
2016-06-29 10:34:04 +0100

    Update Repartee.php

--- Repartee.php
+++ Repartee.php
@@ -1,23 +1,34 @@
 <?php namespace Repartee;
 
-# Repartee Imports
 use Repartee\core\Interfaces\ReparteeFunctions;
 use Repartee\Action\NewMessage;
 use Repartee\Action\GetInboxes;
 use Repartee\Action\GetMessages;
 
+
 class Repartee implements ReparteeFunctions
 {
+    /**
+     * Send a new SMS Message
+     * @return array
+     */
     final public static function newMessage ($data='')
     {
         return new NewMessage($data);
     }
-
+    /**
+     * Get all inboxes for user
+     * @return array
+     */
     public static function getInboxes ($data='')
     {
         return new GetInboxes($data);
     }
-
+    
+    /**
+     * Get all messages within a given inbox
+     * @return array
+     */
     public static function getMessages ($data='')
     {
         return new GetMessages($data);
