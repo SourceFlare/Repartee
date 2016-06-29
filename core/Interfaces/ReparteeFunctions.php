5c553fe6950d19754a3802473e126526526d05f6
Mark Greenall <mark@markgreenall.uk>
2016-06-29 09:23:36 +0100

    Update ReparteeFunctions.php

--- core/Interfaces/ReparteeFunctions.php
+++ core/Interfaces/ReparteeFunctions.php
@@ -4,18 +4,18 @@ namespace Repartee\core\Interfaces;
 interface ReparteeFunctions
 {
     # Inboxes & Messaging
-    public static function GetInboxes ();
-    public static function NewMessage ();
-    public static function GetMessages ();
-    public static function ReadMessage ();
+    public static function getInboxes ();
+    public static function newMessage ();
+    public static function getMessages ();
+    public static function readMessage ();
 
     # Contacts
-    public static function NewContact ();
-    public static function EditContact ();
-    public static function DeleteContact ();
+    public static function newContact ();
+    public static function editContact ();
+    public static function deleteContact ();
 
     # Groups
-    public static function NewGroup ();
-    public static function EditGroup ();
-    public static function DeleteGroup ();
+    public static function newGroup ();
+    public static function editGroup ();
+    public static function deleteGroup ();
 }
